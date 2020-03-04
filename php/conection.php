a7c803a49cc670c245e4de71a5d5f29bc4d741e1
Chiara <chiara.relevat@lilo.org>
2020-03-04 10:02:25 +0100

    connexion a la bdd

---
+++ php/conection.php
@@ -0,0 +1,14 @@
+<?php
+    $servername = "linserv-info-01";;
+    $username = "sj801446";
+    $password = "sj801446"; 
+
+    // Creer connection
+    $conn = new mysqli($servername, $username, $password);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
+?> 
